ec4453887d2810fa7febb16782650583bf8bb66d
Nareshindu <164505135+Nareshindu@users.noreply.github.com>
2024-06-20 14:02:44 +0530

    20-06-2024

---
+++ Sports.java
@@ -0,0 +1,37 @@
+class Sports{
+	public static void main(String[] ipl){
+		String team[]={	"CSK","RCB","MUMBAI","PUNJAB","RAJASTAN","SRH","GUJARAT","LUCKNOW","KOLKATA","DELHI"};
+		String batsmans[]={"Virat Kohli", 
+				"Ruturaj Gaikwad", 
+				"Riyan Parag", 
+				"Travis Head ",
+				"Sanju Samson",
+				"Sai Sudharsan", 
+				"KL Rahul",
+				"Dhoni",
+				"Dube",
+				"Jadeja"};
+		String bowlers[]={
+			"Harshal Patel",
+			"Jasprit Bumrah ",
+			"Varun Chakravarthy",
+			"Yuzvendra Chahal" ,
+			"Tushar Deshpand",
+			"Khaleel Ahmed",
+			"Arshdeep Singh", 
+			"Mukesh Kumar",
+			"Kuldeep Yadav", 
+		"Harshit Rana" };
+		System.out.println("--------------------------------------------------");
+		System.out.println("The IPL Teams are : \n"+team[0]+"\n"+team[1]+"\n"+team[2]+"\n"+team[3]+"\n"+team[4]+"\n"+team[5]+"\n"+team[6]+"\n"+team[7]+"\n"+team[8]+"\n"+team[9]);
+		System.out.println("--------------------------------------------------");
+		
+		System.out.println("**************************************************");
+		System.out.println("The Top 10 Batsmans are : \n"+batsmans[0]+"\n"+batsmans[1]+"\n"+batsmans[2]+"\n"+batsmans[3]+"\n"+batsmans[4]+"\n"+batsmans[5]+"\n"+batsmans[6]+"\n"+batsmans[7]+"\n"+batsmans[8]+"\n"+batsmans[9]);
+		System.out.println("**************************************************");
+		
+		System.out.println("___________________________________________________");
+		System.out.println("The Top 10 Bowlers are : \n"+bowlers[0]+"\n"+bowlers[1]+"\n"+bowlers[2]+"\n"+bowlers[3]+"\n"+bowlers[4]+"\n"+bowlers[5]+"\n"+bowlers[6]+"\n"+bowlers[7]+"\n"+bowlers[8]+"\n"+bowlers[9]);
+		System.out.println("____________________________________________________");
+	}
+}
